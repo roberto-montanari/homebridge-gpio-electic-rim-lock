3470afd3736879a4a3561f3765ba9a763f971c4a
Roberto Montanari <49252166+roberto-montanari@users.noreply.github.com>
2019-04-09 09:20:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ module.exports = function(homebridge) {
 	homebridge.registerAccessory('homebridge-gpio-electic-rim-lock', 'Tiro', ElecticRimLockAccessory);	
 }
 
+function getSerial(){
+	var fs = require('fs');
+	var content = fs.readFileSync('/proc/cpuinfo', 'utf8').split("\n");	
+	var serial = content[content.length-2].split(":");
+	return serial[1].slice(9);
+}
+
 function ElecticRimLockAccessory(log, config) {
 	this.log = log;
 	this.name = config['name'];
@@ -21,7 +28,7 @@ function ElecticRimLockAccessory(log, config) {
 	if (this.duration == null || this.duration % 1 != 0) this.duration = 1000;
 	this.log("Tiro GPIO version: " + this.version);
 	this.log("Switch pin: " + this.pin);
-	this.log("Active time: " + this.duration + "ms");
+	this.log("Active time: " + this.duration + " ms");
 }
 
 ElecticRimLockAccessory.prototype = {
@@ -31,7 +38,7 @@ ElecticRimLockAccessory.prototype = {
 		informationService
     			.setCharacteristic(Characteristic.Manufacturer, "Roberto Montanari")
 			.setCharacteristic(Characteristic.Model, "Tiro GPIO")
-			.setCharacteristic(Characteristic.SerialNumber, "000-0"+this.pin)
+			.setCharacteristic(Characteristic.SerialNumber, getSerial() + this.pin)
 			.setCharacteristic(Characteristic.FirmwareRevision, this.version);
 
 		let lockMechanismService = new Service.LockMechanism(this.name);
@@ -66,7 +73,7 @@ ElecticRimLockAccessory.prototype = {
 	setLockTargetState: function(state, callback) {
 		if (state == 0) {
 			this.writePin(1);
-			this.log("Wait " + this.duration + " ms");
+			this.log("Wait for " + this.duration + " ms");
 			var self = this;
 			setTimeout(function(){self.writePin(0)}, this.duration);
 			this.log("Set state to open");
@@ -74,14 +81,14 @@ ElecticRimLockAccessory.prototype = {
 			callback(null);
 		} else {
 			this.writePin(0);
-			this.log("Set state to close");
+			this.log("Set state to closed");
 			this.lastLockTargetState = 1;
 			callback(null);
 		}						
 	},
 
 	writePin: function(val) {	
-		this.log("Turning " + (val == 0 ? "off" : "on") + " pin #" + this.pin);
+		this.log("Turning " + (val == 0 ? "off" : "on") + " pin " + this.pin);
 		rpio.open(this.pin, rpio.OUTPUT);
 		rpio.write(this.pin, val);
 	}
