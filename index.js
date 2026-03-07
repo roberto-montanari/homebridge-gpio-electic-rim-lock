6680638cf989955fc8ad1e2dd0bbcbef64a435c5
Roberto Montanari <49252166+roberto-montanari@users.noreply.github.com>
2026-03-07 16:16:51 +0100

    Added error handling

--- index.js
+++ index.js
@@ -1,99 +1,122 @@
 "use strict";
+
 var Service, Characteristic;
 var rpio = require('rpio');
 
 module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
 
-	homebridge.registerAccessory('homebridge-gpio-electic-rim-lock', 'Tiro', ElecticRimLockAccessory);	
-}
+    homebridge.registerAccessory(
+        'homebridge-gpio-electic-rim-lock',
+        'Tiro',
+        ElecticRimLockAccessory
+    );
+};
 
-function getSerial(){
-	var fs = require('fs');
-  	var cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
-  	var match = cpuinfo.match(/Serial\s*:\s*([a-f0-9]+)/i);
-  	return match ? match[1] : null;
+function getSerial() {
+    var fs = require('fs');
+    var cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
+    var match = cpuinfo.match(/Serial\s*:\s*([a-f0-9]+)/i);
+    return match ? match[1] : null;
 }
 
 function ElecticRimLockAccessory(log, config) {
-	this.log = log;
-	this.name = config['name'];
-	this.pin = config['pin'];
-	this.duration = config['duration'];
-	this.version = require('./package.json').version;
-	this.lastLockTargetState = 1;
+    this.log = log;
+    this.name = config['name'];
+    this.pin = config['pin'];
+    this.duration = config['duration'];
+    this.version = require('./package.json').version;
+    this.lastLockTargetState = 1;
 
-	if (!this.name || !this.pin) {
-		this.log.warn("You must provide a valid config value for name and pin.");
-		this.disabled = true;
-		return;
-	}
-	if (this.duration == null || this.duration % 1 != 0) this.duration = 500;
-	this.log("Tiro GPIO version: " + this.version);
-	this.log("Switch pin: " + this.pin);
-	this.log("Active time: " + this.duration + " ms");
+    // Check configuration
+    if (!this.name || !this.pin) {
+        this.log.warn("⚠ RimLock not configured correctly: name or pin missing. Plugin is disabled.");
+        this.disabled = true;
+        return;
+    }
+
+    if (this.duration == null || this.duration % 1 !== 0) this.duration = 500;
+
+    this.log("Tiro GPIO version: " + this.version);
+    this.log("Switch pin: " + this.pin);
+    this.log("Active time: " + this.duration + " ms");
 }
 
 ElecticRimLockAccessory.prototype = {
-  
-	getServices: function() {	
-		let informationService = new Service.AccessoryInformation();
-		informationService
-    		.setCharacteristic(Characteristic.Manufacturer, "Roberto Montanari")
-			.setCharacteristic(Characteristic.Model, "Tiro GPIO")
-			.setCharacteristic(Characteristic.SerialNumber, getSerial() + this.pin)
-			.setCharacteristic(Characteristic.FirmwareRevision, this.version);
 
-		let lockMechanismService = new Service.LockMechanism(this.name);
-		lockMechanismService
-    		.getCharacteristic(Characteristic.LockCurrentState)
-    		.on('get', this.getLockCurrentState.bind(this));		
-		lockMechanismService
-    		.getCharacteristic(Characteristic.LockTargetState)
-    		.on('get', this.getLockTargetState.bind(this))
-    		.on('set', this.setLockTargetState.bind(this));
+    getServices: function() {
+        if (this.disabled) {
+            this.log.warn("Plugin disabled, no services will be registered.");
+            return [];
+        }
 
-		this.informationService = informationService;
-		this.lockMechanismService = lockMechanismService;
+        let informationService = new Service.AccessoryInformation();
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Roberto Montanari")
+            .setCharacteristic(Characteristic.Model, "Tiro GPIO")
+            .setCharacteristic(Characteristic.SerialNumber, getSerial() + this.pin)
+            .setCharacteristic(Characteristic.FirmwareRevision, this.version);
 
-		return [informationService, lockMechanismService];
-	},
+        let lockMechanismService = new Service.LockMechanism(this.name);
+        lockMechanismService
+            .getCharacteristic(Characteristic.LockCurrentState)
+            .on('get', this.getLockCurrentState.bind(this));
+        lockMechanismService
+            .getCharacteristic(Characteristic.LockTargetState)
+            .on('get', this.getLockTargetState.bind(this))
+            .on('set', this.setLockTargetState.bind(this));
 
-	getLockCurrentState: function(callback) {
-		if (this.lastLockTargetState == 0) {
-			this.lastLockTargetState = 1;
-			callback(null, 0);
-		} else {
-			callback(null, 1);
-		}
-	},
+        this.informationService = informationService;
+        this.lockMechanismService = lockMechanismService;
 
-	getLockTargetState: function(callback) {
-		this.lastLockTargetState = 1;
-		callback(null, 1);
-	},
+        return [informationService, lockMechanismService];
+    },
 
-	setLockTargetState: function(state, callback) {
-		if (state == 0) {
-			this.writePin(1);
-			this.log("Wait for " + this.duration + " ms");
-			var self = this;
-			setTimeout(function(){self.writePin(0)}, this.duration);
-			this.log("Set state to open");
-			this.lastLockTargetState = 0;
-			callback(null);
-		} else {
-			this.writePin(0);
-			this.log("Set state to closed");
-			this.lastLockTargetState = 1;
-			callback(null);
-		}						
-	},
+    getLockCurrentState: function(callback) {
+        if (this.lastLockTargetState === 0) {
+            this.lastLockTargetState = 1;
+            callback(null, 0);
+        } else {
+            callback(null, 1);
+        }
+    },
 
-	writePin: function(val) {	
-		this.log("Turning " + (val == 0 ? "off" : "on") + " pin " + this.pin);
-		rpio.open(this.pin, rpio.OUTPUT);
-		rpio.write(this.pin, val);
-	}
-}
+    getLockTargetState: function(callback) {
+        this.lastLockTargetState = 1;
+        callback(null, 1);
+    },
+
+    setLockTargetState: function(state, callback) {
+        if (this.disabled) {
+            this.log.warn("Plugin disabled, command ignored.");
+            return callback(null);
+        }
+
+        if (state === 0) { // Unlock
+            this.writePin(1);
+            this.log("Waiting for " + this.duration + " ms");
+            setTimeout(() => this.writePin(0), this.duration);
+            this.log("Lock state set to OPEN");
+            this.lastLockTargetState = 0;
+            callback(null);
+        } else { // Lock
+            this.writePin(0);
+            this.log("Lock state set to CLOSED");
+            this.lastLockTargetState = 1;
+            callback(null);
+        }
+    },
+
+    writePin: function(val) {
+        if (this.disabled) return;
+
+        try {
+            rpio.open(this.pin, rpio.OUTPUT, rpio.LOW);
+            rpio.write(this.pin, val);
+            this.log("GPIO pin " + this.pin + " set " + (val === 0 ? "OFF" : "ON"));
+        } catch (err) {
+            this.log.error("GPIO error on pin " + this.pin + ": " + err.message);
+        }
+    }
+};
